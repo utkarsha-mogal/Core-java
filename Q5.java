4f8153946569b7d3c8e727557da49087a06b0d7f
utkarsha-mogal <66870237+utkarsha-mogal@users.noreply.github.com>
2020-09-18 22:26:01 +0530

    Add files via upload

---
+++ Q5.java
@@ -0,0 +1,12 @@
+
+class cmdargs{
+  public static void main(strings args[]){
+// java Cmdargs "ram kumar" "utkarsha mogal" as argument in cmd
+
+  string s1 = args[0];
+  strings s2 = args[1];
+
+           system.out.println(s1);
+           system.out.println(s2);
+}
+}
