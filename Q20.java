466459f065bdee3dbf9d95381b86f6abb847dd2a
utkarsha-mogal <66870237+utkarsha-mogal@users.noreply.github.com>
2020-09-20 12:15:57 +0530

    Add files via upload

---
+++ Q20.java
@@ -0,0 +1,19 @@
+import java.util.*;
+ public class Q20{
+  public static void main(string args[]){
+    Scanner sc =  new Scanner(System.in);
+   int num1 =  sc.nextInt();
+    int num2 = sc.nextInt();
+ int flag;
+ while(num1 < = num2){
+ flag=0;
+ for( int i=2 ; i<=num1/2 ; i++){
+ if(num%i == 0){
+
+flag = 1;
+break;
+
+ if(flag ==0){
+      sysytem.out.print(num1+" ");
+                     }
+   
