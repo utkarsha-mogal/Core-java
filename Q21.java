3c1f33de29649ff00132d60b8e4636df8e564585
utkarsha-mogal <66870237+utkarsha-mogal@users.noreply.github.com>
2020-09-20 12:17:45 +0530

    Add files via upload

---
+++ Q21.java
@@ -0,0 +1,17 @@
+     import java.util.*;
+      public class Q21{
+     public static void main(string args[]){
+       Scanner sc =  new Scanner(System.in);
+
+   int arr[] =  new int[10];
+  int  sum=0;
+
+  for(int i=0; i<arr.length; i++ ) {
+	arr[i] = sc.nextInt();
+                  sun+=arr[i];
+
+    float  avg = sum/arr.length;
+    System.out.println("sum is "+sum+ " average is "+avg);
+ }
+}
+}
