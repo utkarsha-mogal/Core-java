523c833a214a6ee9ddf12c67f515335d40f6e82e
utkarsha-mogal <66870237+utkarsha-mogal@users.noreply.github.com>
2020-09-20 12:14:43 +0530

    Add files via upload

---
+++ Q18.java
@@ -0,0 +1,13 @@
+import java.util.*
+   public class Q18{
+   public static void main(string ar[])	
+ {
+  Scanner x = new Scanner(system.in);
+  system.out.println("Enter the number");
+ int n = x.next Int();
+ int f = 0;
+ for(int i=2 ; i < n/2 ; i++)
+{
+ 	if(n % i == 0)
+       {
+	
