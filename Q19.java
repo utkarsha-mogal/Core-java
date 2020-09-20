5c3f758b51e81907a112af87789da364b35bf99a
utkarsha-mogal <66870237+utkarsha-mogal@users.noreply.github.com>
2020-09-20 12:15:25 +0530

    Add files via upload

---
+++ Q19.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+   public class Q19{
+   public static void main(String args[]){
+  Scanner x = new Scanner(System.in);
+  System.out.println("Enter the number");
+ int n =  x.next int();
+  int num = 2;
+ int sum= 0;
+   for(int i=0 ; i < n ; i++)
+ {
+  num+=10;
+  if(i == n-1)
+{
+ System.out.println(num);
+break;
+}
