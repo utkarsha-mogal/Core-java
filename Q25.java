aba63784a4c036116f5c8207c97571916e321713
utkarsha-mogal <66870237+utkarsha-mogal@users.noreply.github.com>
2020-09-20 15:00:49 +0530

    Add files via upload

---
+++ Q25.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ class Q25
+{
+    public static void main(String[] args) 
+    {
+        int n, sumE = 0, sumO = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number of elements in array:");
+        n = s.nextInt();
+        int[] a = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for(int i = 0; i < 5; i++)
+        {
+            a[i] = s.nextInt();
+        }
+        for(int i = 0; i < 5; i++)
+        {
+            if(a[i] % 2 == 0)
+            {
+                sumE = sumE + a[i];
+            }
+            else
+            {
+                sumO = sumO + a[i];
+            }
+        }
+        System.out.println("Sum of Even Numbers  "+sumE);
+        System.out.println("Sum of Odd Numbers  "+sumO);
+    }
+}
