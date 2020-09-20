780f66a22c6295a9bfebe5fa7cc3061d5736b7ec
utkarsha-mogal <66870237+utkarsha-mogal@users.noreply.github.com>
2020-09-20 12:18:14 +0530

    Add files via upload

---
+++ Q22.java
@@ -0,0 +1,8 @@
+import java.util.*;
+      public class Q21{
+     public static void main(string args[]){
+       Scanner sc =  new Scanner(System.in);
+   
+  int arr[] = new int[10];
+  int sum=0;
+     for(i=0; i<arr.length; i++)
