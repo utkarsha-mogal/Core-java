ab3a3f732b6c729d395e7bcb2ed5730301bcbf79
utkarsha-mogal <66870237+utkarsha-mogal@users.noreply.github.com>
2020-09-20 14:44:18 +0530

    Add files via upload

---
+++ Q23.java
@@ -0,0 +1,17 @@
+
+public class Q23 {  
+    public static void main(String[] args) {  
+ 
+        int [] arr = new int [] {6, 7, 9, 10, 11};  
+        System.out.println("Original array: ");  
+        for (int i = 0; i < arr.length; i++) {  
+            System.out.print(arr[i] + " ");  
+        }  
+        System.out.println();  
+        System.out.println("Array in reverse order ");  
+        
+        for (int i = arr.length-1; i >= 0; i--) {  
+            System.out.print(arr[i] + " ");  
+        }  
+    }  
+}  
